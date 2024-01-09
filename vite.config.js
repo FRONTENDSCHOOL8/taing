c98773316aed280b70c3cd68464cc862b92ab71e
ksyee <tjssud0647@gmail.com>
2024-01-09 10:36:41 +0900

    [docs] vite.config.js에 html파일 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,11 @@ export default defineConfig({
     outDir: 'docs',
     rollupOptions: {
       input: {
-        welcome: resolve(__dirname, 'index.html'),
-        landingPage: resolve(__dirname, 'src/pages/landingPage/index.html'),
+        landingPage: resolve(__dirname, 'index.html'),
         mainPage: resolve(__dirname, 'src/pages/mainPage/index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        selectProfile: resolve(__dirname, 'src/pages/selectProfile/index.html'),
+        editProfile: resolve(__dirname, 'src/pages/editProfile/index.html'),
         header: resolve(__dirname, 'src/components/header.html'),
         footer: resolve(__dirname, 'src/components/footer.html'),
       },
