38d239cdd48a79f849dcc34f4193220d4e1ed698
ksyee <tjssud0647@gmail.com>
2024-01-11 15:47:22 +0900

    [docs] findPassword.js 최신화

---
+++ src/pages/findPassword/findPassword.js
@@ -0,0 +1,44 @@
+import { getData } from '/src/util/crud';
+import { pb } from '/src/api/pocketBase';
+
+const inputUserId = document.querySelector('#userId');
+const form = document.querySelector('form');
+const submitButton = document.querySelector('button[type="submit"]');
+
+inputUserId.addEventListener('input', (e) => {
+  const input = e.target;
+
+  if (input.value.length > 2) {
+    submitButton.classList.remove('bg-button-submit-default');
+    submitButton.classList.add('bg-button-submit-active');
+  } else {
+    submitButton.classList.remove('bg-button-submit-active');
+    submitButton.classList.add('bg-button-submit-default');
+  }
+});
+
+form.addEventListener('submit', findId);
+
+async function findId(e) {
+  e.preventDefault();
+
+  const userIdValue = inputUserId.value;
+
+  try {
+    const res = await getData('users', {
+      filter: `userId='${userIdValue}'`,
+    });
+
+    if (res.length > 0) {
+      console.log(res[0].email);
+      await pb.collection('users').requestPasswordReset(res[0].email);
+
+      alert(`${res[0].email}메일로 비밀번호 재설정 링크를 발송했습니다.`);
+      location.href = '/src/pages/login/';
+    } else {
+      alert('일치하는 아이디가 없습니다.');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
