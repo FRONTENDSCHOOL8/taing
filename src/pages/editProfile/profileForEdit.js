79ae1f2000e397441e5162b748c4a8410e009c68
mondimhaus <mondimhaus@naver.com>
2024-01-15 11:10:00 +0900

    [feat] 프로필 기능 추가

---
+++ src/pages/editProfile/profileForEdit.js
@@ -0,0 +1,7 @@
+/*
+  * 프로필 수정: patch
+    - 사진 수정
+    - input type="file" : 파일 업로드
+    - submit button : 서버로 파일 전송
+  * 프로필 추가: patch
+  */
--- src/pages/selectProfile/selectProfile.js
+++ src/pages/selectProfile/selectProfile.js
@@ -2,104 +2,74 @@ import { getData } from '/src/util/crud';
 import { getImageURL } from '/src/util/getImageURL';
 import { insertTemplate } from '/src/util/insertTemplate';
 
-/* 
-  todo 버튼 수정
-  todo 활성화 프로필
- */
+// 클릭한 프로필버튼의 alt.nickname과 서버의 nickname 을 연결
 
-//! DB Data
-// 로그인된 계정의 유저 데이터 가져오기
-// (수정하기..)
-const loginUser = await JSON.parse(localStorage.getItem('auth'));
-const userData = loginUser.model;
+// 프로필 데이터 추출
+const extractProfileData = (userData) => {
+  let profileData = [];
+  for (let i = 1; i <= 4; i++) {
+    const profileImage = `profile_${i}`;
+    const profileNickname = `profile_${i}_nickname`;
 
-// const userData = await getData('users', {
-//   filter: 'userId = "mnd-prince"',
-// });
+    if (userData[profileImage] || userData[profileNickname]) {
+      profileData.push({
+        profileImage: userData[profileImage],
+        profileNickname: userData[profileNickname],
+        collectionId: userData.collectionId,
+        id: userData.id,
+      });
+    }
+  }
+  console.log(profileData);
+  return profileData;
+};
 
-//! rendering
-userData.forEach((item) => {
-  const template = /* html */ `
-    <li class="profileList">
-      <button
-        class="profileButton relative mb-12pxr max-w-114pxr tablet:max-w-132pxr desktop:max-w-234pxr"
-      >
-        <img
-          src="${getImageURL(item, 'profile_1')}"
-          alt="${item.profile_1_nickname} 프로필 사진"
-          class="rounded opacity-50"
-        />
-        <img
-          src="/public/assets/profile/Icon/lock.svg"
-          alt="lock"
-          class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform desktop:w-60pxr"
-        />
-      </button>
-      <p class="text-center text-12pxr text-taing-2">${
-        item.profile_1_nickname
-      }</p>
-    </li>
-    <li class="profile">
-      <button
-        class="profileButton relative mb-12pxr max-w-114pxr tablet:max-w-132pxr desktop:max-w-234pxr"
-      >
-        <img
-          src="${getImageURL(item, 'profile_2')}"
-          alt="${item.profile_2_nickname} 프로필 사진"
-          class="rounded opacity-50"
-        />
-        <img
-          src="/public/assets/profile/Icon/lock.svg"
-          alt="lock"
-          class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform desktop:w-60pxr"
-        />
-      </button>
-      <p class="text-center text-12pxr text-taing-2">${
-        item.profile_2_nickname
-      }</p>
-    </li>
-    <li class="profile">
-      <button
-        class="profileButton relative mb-12pxr max-w-114pxr tablet:max-w-132pxr desktop:max-w-234pxr"
-      >
-        <img
-          src="${getImageURL(item, 'profile_3')}"
-          alt="${item.profile_3_nickname} 프로필 사진"
-          class="rounded opacity-50"
-        />
-        <img
-          src="/public/assets/profile/Icon/lock.svg"
-          alt="lock"
-          class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform desktop:w-60pxr"
-        />
-      </button>
-      <p class="text-center text-12pxr text-taing-2">${
-        item.profile_3_nickname
-      }</p>
-    </li>
-    <li class="profile">
-      <button
-        class="profileButton relative mb-12pxr max-w-114pxr tablet:max-w-132pxr desktop:max-w-234pxr"
-      >
-        <img
-          src="${getImageURL(item, 'profile_4')}"
-          alt="${item.profile_4_nickname} 프로필 사진"
-          class="rounded opacity-100"
-        />
-      </button>
-      <p class="text-center text-12pxr text-taing-2">${
-        item.profile_4_nickname
-      }</p>
-    </li>`;
-  insertTemplate('.profile-photo > ul', template);
-});
-
-//! 프로필 클릭 시 메인 페이지 이동
-// 수정하기..
-const profileButtons = document.querySelectorAll('.profileButton');
-
-profileButtons.forEach((item) => {
-  item.addEventListener('click', () => {
-    window.location.href = '/src/pages/mainPage/index.html';
+// 프로필 렌더링
+const renderProfile = (profileData) => {
+  profileData.forEach((item) => {
+    const template = `
+      <li class="profileList">
+        <button
+          class="profileButton relative mb-12pxr max-w-114pxr tablet:max-w-132pxr desktop:max-w-234pxr"
+        >
+          <img
+            src="${getImage(item, item.profileImage)}"
+            alt="${item.profileNickname} 프로필 사진"
+            class="rounded opacity-50"
+          />
+          <img
+            src="/public/assets/profile/Icon/lock.svg"
+            alt="lock"
+            class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform desktop:w-60pxr"
+          />
+        </button>
+        <p class="text-center text-12pxr text-taing-2">${
+          item.profileNickname
+        }</p>
+      </li>`;
+    insertTemplate('.profile-photo > ul', template);
   });
-});
+};
+
+// 초기화
+const init = async () => {
+  const loginUser = await JSON.parse(localStorage.getItem('auth'));
+  const userData = loginUser.model;
+  const profileData = extractProfileData(userData);
+  renderProfile(profileData);
+
+  const profileButtons = document.querySelectorAll('.profileButton');
+  profileButtons.forEach((item) => {
+    item.addEventListener('click', () => {
+      window.location.href = '/src/pages/mainPage/index.html';
+    });
+  });
+};
+
+init();
+
+function getImage(item, fileName) {
+  return `${import.meta.env.VITE_PB_URL}/api/files/${item.collectionId}/${
+    item.id
+  }/${fileName}`;
+}
