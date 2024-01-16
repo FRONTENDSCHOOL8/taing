7a31bd12f850e37707b0c3f57bc9ec40cc80553a
ksyee <tjssud0647@gmail.com>
2024-01-16 10:25:41 +0900

    [docs] 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -7,36 +7,33 @@ export default defineConfig({
     outDir: 'docs',
     rollupOptions: {
       input: {
-        header: resolve(__dirname, '/src/components/header.html'),
-        footer: resolve(__dirname, '/src/components/footer.html'),
-        landingPage: resolve(__dirname, '/index.html'),
-        login: resolve(__dirname, '/src/pages/login/index.html'),
-        regist: resolve(__dirname, '/src/pages/regist/index.html'),
-        mainPage: resolve(__dirname, '/src/pages/mainPage/index.html'),
-        detailPage: resolve(__dirname, '/src/pages/detailPage/index.html'),
-        selectProfile: resolve(
-          __dirname,
-          '/src/pages/selectProfile/index.html'
-        ),
-        editProfile: resolve(__dirname, '/src/pages/editProfile/index.html'),
-        findId: resolve(__dirname, '/src/pages/findId/index.html'),
-        findPassword: resolve(__dirname, '/src/pages/findPassword/index.html'),
-        searchPage: resolve(__dirname, '/src/pages/searchPage/index.html'),
+        header: resolve(__dirname, 'src/components/header.html'),
+        footer: resolve(__dirname, 'src/components/footer.html'),
+        landingPage: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        regist: resolve(__dirname, 'src/pages/regist/index.html'),
+        mainPage: resolve(__dirname, 'src/pages/mainPage/index.html'),
+        detailPage: resolve(__dirname, 'src/pages/detailPage/index.html'),
+        selectProfile: resolve(__dirname, 'src/pages/selectProfile/index.html'),
+        editProfile: resolve(__dirname, 'src/pages/editProfile/index.html'),
+        findId: resolve(__dirname, 'src/pages/findId/index.html'),
+        findPassword: resolve(__dirname, 'src/pages/findPassword/index.html'),
+        searchPage: resolve(__dirname, 'src/pages/searchPage/index.html'),
         logoutModal: resolve(
           __dirname,
-          '/src/pages/modal/logoutModal/index.html'
+          'src/pages/modal/logoutModal/index.html'
         ),
         mainPageModal: resolve(
           __dirname,
-          '/src/pages/modal/mainPageModal/index.html'
+          'src/pages/modal/mainPageModal/index.html'
         ),
         userMenuModal: resolve(
           __dirname,
-          '/src/pages/modal/userMenuModal/index.html'
+          'src/pages/modal/userMenuModal/index.html'
         ),
         withdrawalModal: resolve(
           __dirname,
-          '/src/pages/modal/withdrawalModal/index.html'
+          'src/pages/modal/withdrawalModal/index.html'
         ),
       },
     },
